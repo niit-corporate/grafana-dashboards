5d9dcf410f0ab1d12b0e7504a5b81dee54139cf7
Roman Misyurin <roman.misyurin@percona.com>
2020-05-24 11:54:19 +0300

    PMM-544 styles fixes and grafana packages updated to 7.0.0

--- pmm-app/src/react-plugins-deps/components/helpers/api.test.ts
+++ pmm-app/src/react-plugins-deps/components/helpers/api.test.ts
@@ -2,10 +2,11 @@ import { apiRequest } from './api';
 import axios from 'axios';
 jest.mock('axios');
 jest.mock('../../../react-plugins-deps/components/helpers/notification-manager', () => () => ({}));
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('GET tests', () => {
   it('should return data', async () => {
-    axios.get.mockResolvedValue({ data: 'some data' });
+    mockedAxios.get.mockResolvedValue({ data: 'some data' });
     const result = await apiRequest.get('/test/path', { params: { key: 'value' } });
     await expect(result).toEqual('some data');
   });
@@ -13,7 +14,7 @@ describe('GET tests', () => {
 
 describe('POST tests', () => {
   it('should return data', async () => {
-    axios.post.mockResolvedValue({ data: 'some data' });
+    mockedAxios.post.mockResolvedValue({ data: 'some data' });
     const result = await apiRequest.post('/test/path', { key: 'value' });
     await expect(result).toEqual('some data');
   });
@@ -21,7 +22,7 @@ describe('POST tests', () => {
 
 describe('PATCH tests', () => {
   it('should return data', async () => {
-    axios.patch.mockResolvedValue({ data: 'some data' });
+    mockedAxios.patch.mockResolvedValue({ data: 'some data' });
     const result = await apiRequest.patch('/test/path', { key: 'value' });
     await expect(result).toEqual('some data');
   });
@@ -29,7 +30,7 @@ describe('PATCH tests', () => {
 
 describe('DELETE tests', () => {
   it('should return data', async () => {
-    axios.delete.mockResolvedValue({ data: 'some data' });
+    mockedAxios.delete.mockResolvedValue({ data: 'some data' });
     const result = await apiRequest.delete('/test/path');
     await expect(result).toEqual('some data');
   });
